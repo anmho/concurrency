db71790f3780769a9ee906628010e2fcee7be56c
Andrew Ho <andyminhtuanho@gmail.com>
2025-12-07 19:29:13 -0800

    producer consumer

---
+++ cmd/channels/producer_consumer/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	stringStream := make(chan string)// unbuffered channel
+
+	// producer
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		stringStream <- "hello"
+		wg.Done()
+	}()
+
+	// consumer. will wait until theres a value in the channel to get
+	fmt.Println(<-stringStream)
+}
