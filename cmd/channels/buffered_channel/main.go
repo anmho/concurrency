5c8c0aa7568f3b6ffaa2e66f0a5692bac217e5c7
Andrew Ho <andyminhtuanho@gmail.com>
2025-12-07 20:16:13 -0800

    buffered channel

--- cmd/channels/buffered_channel/main.go
+++ cmd/channels/buffered_channel/main.go
@@ -28,4 +28,34 @@ func main() {
 	for i := range intStream {
 		fmt.Fprintf(&stdoutBuff, "Received %v", i)
 	}
+}package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var stdoutBuff bytes.Buffer
+	defer stdoutBuff.WriteTo(os.Stdout)
+
+
+	// createa buffered chan of 4
+	intStream := make(chan int, 4)
+
+	// fire off gorotutine
+	go func() {
+		defer close(intStream)
+		defer fmt.Fprintln(&stdoutBuff, "Producer Done.")
+		for i := 0; i < 5; i++ {
+			fmt.Fprintf(&stdoutBuff, "Sending: %d\n", i)
+			intStream <- i
+		}
+	}()
+
+	// gets items until channel is closed. then breaks the loop
+	for i := range intStream {
+		fmt.Fprintf(&stdoutBuff, "Received %v", i)
+	}
 }
