7f61451940c7d3cb70790bc67b3d65f514406bef
Andrew Ho <andyminhtuanho@gmail.com>
2025-12-11 00:06:55 -0800

    add ticker example

---
+++ cmd/ticker/background/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+
+func backgroundJob(ctx context.Context) {
+	t := time.NewTicker(time.Second)
+	for {
+		select {
+		case <-t.C:
+			fmt.Println("tick")
+		case <-ctx.Done():
+			fmt.Println("boom")
+			return
+		}
+	}
+}
+
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	
+	go func () {
+		// defer wg.Done()
+		backgroundJob(ctx)
+		
+	}()
+	wg.Wait()
+}
